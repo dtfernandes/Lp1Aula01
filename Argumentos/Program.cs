8eb0b84949dd1f4b8bbb3a624a0de05ad1a1ac56
Daniel Fernandes <danielteotonio@sapo.pt>
2020-02-27 10:27:52 +0000

    Add Comments and Xml tags

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -6,12 +6,12 @@ namespace Argumentos
     {
         static void Main(string[] args)
         {
-
-            for(int i = 0; i < args.Length; i++){
+            //Prints all arguments given in the console
+            for (int i = 0; i < args.Length; i++)
+            {
                 Console.WriteLine(args[i]);
             }
 
-            
             /*
             Different solution
             foreach(string o in args){
--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,15 +2,28 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// Main Program Class
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// The Program starts here
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bye World!");
+
+            /*Quirky stuff
+
             Console.WriteLine("Nome: ");
             string c = Console.ReadLine();
             Console.WriteLine($"Hello {c}!", c);
 
             Console.WriteLine($"Bye {c}", c);
+            */
         }
     }
 }
