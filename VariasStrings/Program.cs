97b29af456470400cce9052262eee5012225dcc7
Daniel Fernandes <danielteotonio@sapo.pt>
2020-02-27 11:43:52 +0000

    Add VariasStrings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string cena1 = @" "" "" "" "" "" "" "" "" "" """;
+            string cena2 = "o Marco é\ttriste";
+
+
+            Console.WriteLine(cena1);
+            Console.WriteLine(cena2);
+
+        }
+    }
+}
