194bb1b0b240dd815bf23111b2505dc1cf7ef41f
Daniel Fernandes <danielteotonio@sapo.pt>
2020-02-27 11:05:21 +0000

    Update TiposVarios with Real numbers

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -6,15 +6,28 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+
+            //Declare Integer vars 
             int inteiro;
-            uint unsignedInt = 9u; 
+            uint unsignedInt = 9u;
             byte test = 0b00000010;
 
             inteiro = 0;
- 
+
+            //Print Integer vars 
             Console.WriteLine(test);
             Console.WriteLine(unsignedInt);
             Console.WriteLine(inteiro);
+
+            //Declare Real vars 
+            float myFloat = 99.9999f;
+            double myDouble = 1e-20;
+
+            //Print Real vars 
+            Console.WriteLine(myFloat);
+            Console.WriteLine(myDouble);
+
+
         }
     }
 }
